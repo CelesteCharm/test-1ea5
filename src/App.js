146f04bc04ea6e43749d0ec4464b424b14f82774
CelesteCharm <beauciel00@gmail.com>
2023-11-25 21:16:07 +0900

    format code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import logo from "./logo.svg";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-        <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+          <Button variant="primary">Primary</Button>{" "}
+          <Button variant="secondary">Secondary</Button>{" "}
+          <Button variant="success">Success</Button>{" "}
+          <Button variant="warning">Warning</Button>{" "}
+          <Button variant="danger">Danger</Button>{" "}
+          <Button variant="info">Info</Button>{" "}
+          <Button variant="light">Light</Button>{" "}
+          <Button variant="dark">Dark</Button>
+          <Button variant="link">Link</Button>
         </p>
         <a
           className="App-link"
