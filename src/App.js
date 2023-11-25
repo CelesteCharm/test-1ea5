214606e76c22eb8b68177f1d3c370d2c2a1f14b6
CelesteCharm <beauciel00@gmail.com>
2023-11-25 21:42:44 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import logo from "./logo.svg";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 2);
+  }
+  function handleClick2() {
+    setCount(count - 2);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          <Button variant="primary">Primary</Button>{" "}
-          <Button variant="secondary">Secondary</Button>{" "}
-          <Button variant="success">Success</Button>{" "}
-          <Button variant="warning">Warning</Button>{" "}
-          <Button variant="danger">Danger</Button>{" "}
-          <Button variant="info">Info</Button>{" "}
-          <Button variant="light">Light</Button>{" "}
-          <Button variant="dark">Dark</Button>
-          <Button variant="link">Link</Button>
+
+          <Button onClick={handleClick} variant="primary">
+            +
+          </Button>{" "}
+          <Button onClick={handleClick2} variant="secondary">
+            -
+          </Button>{" "}
+          {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
